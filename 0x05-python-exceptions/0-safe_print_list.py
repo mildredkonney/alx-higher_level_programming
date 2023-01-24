01fd840ab4f36f2dfb4c5cfa511b73e55a2b7e20
mildredkonney <mildredkonney9@gmail.com>
2023-01-24 07:52:16 -0800

    except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
              print("{}".format(my_list[i]), end="")
              total += 1
      except IndexError:
-         pass
+         break
      print()
-     return total
+     return(total)
 
